1030f6892ce870d23d3688dbb47c997310d106bb
Youngpro95 <mmyoung95@naver.com>
2022-07-29 22:24:54 +0900

    useRef

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,23 +2,22 @@ import React from "react";
 
 
 // 1부터 100까지 들어있는 arr
-const arr = Array.from(Array(100), (_, i) => i + 1);
+// const arr = Array.from(Array(100), (_, i) => i + 1);
 
 const App = () => {
-    const [result, setResult] = React.useState(0); 
-    const [left, leftVal] = React.useState(0); 
-    const [right, rightVal] = React.useState(0); 
+  const [inputTest , inputVal] = React.useState(0);
+  const input = React.useRef(null); 
+  const handleClick = () => { 
+    if (input.current) { 
+      input.current.value = ''; 
+    } 
+  } 
   return ( 
-    <> 
-      <input type='number' onChange={(e)=>{leftVal(parseInt(e.target.value))}}/> 
-      + 
-      <input type='number' onChange={(e)=>{rightVal(parseInt(e.target.value))}}/> 
-      = 
-      <input type='number' disabled value={result} /> 
-      <button type='button' onClick={()=>{
-        setResult(left + right)
-      }}>계산</button> 
-    </> 
+    <div> 
+      <div>현재 value는 {inputTest} 입니다.</div>
+      <input type="text" ref={input} onChange={(e)=>inputVal(e.target.value)} />
+      <button type="button" onClick={handleClick}>Click to Reset</button> 
+    </div> 
   ); 
 } 
 export default App;
